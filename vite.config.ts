7c40fb11bdb1b70a6d4fadca5b12514f29c731b5
Mohamed Aziz Gmaty <azizgmt817@gmail.com>
2026-05-25 13:22:09 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
 export default defineConfig({
+  cloudflare: false,
   tanstackStart: {
     server: {
-      entry: "server",
       preset: "vercel" // <-- Add this line to override the Cloudflare default
     },
   },
